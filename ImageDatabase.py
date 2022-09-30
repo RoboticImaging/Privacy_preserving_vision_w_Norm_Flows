8a505968f4e9880b479baa951ce24a12af2d5436
Adam Taras <ataras2@gmail.com>
2022-09-30 22:04:50 +1000

    image database can transform all images and save

--- ImageDatabase.py
+++ ImageDatabase.py
@@ -1,6 +1,9 @@
 
 
 import os
+import cv2
+import numpy as np
+from ImageTransform import Image_Transform
 
 class Image_Database:
     # reads images recursively 
@@ -11,16 +14,27 @@ class Image_Database:
         self.input_path = input_path
         self.output_path = output_path
     
-    def transform_and_save(self, list_of_transforms):
+    def transform_and_save(self, list_of_transforms, output_ftype = 'png'):
+        imgIdx = 0
         for subdir, dirs, files in os.walk(self.input_path):
             for file in files:
                 img = np.array(cv2.imread(os.path.join(subdir, file)))
                 for transform in list_of_transforms:
-                    
+                    img = transform(img)
+                cv2.imwrite(os.path.join(self.output_path, f"{imgIdx}.{output_ftype}"), img)
+                imgIdx += 1
+                
 
 
 
 if __name__ == "__main__":
-    id = Image_Database('data\Extracted', 'outputs\LSUN')
-    id.transform_and_save([])
+    id = Image_Database('data\Extracted', 'outputs/LSUN/64x64')
+
+    new_size = 64
+
+    id.transform_and_save([
+        Image_Transform.centre_crop,
+        Image_Transform.color2monochrome,
+        lambda img: Image_Transform.resize(img,[new_size, new_size])
+    ])
 
