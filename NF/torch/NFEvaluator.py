b274fe3461160a21f075b8b64bb7ad0063edf675
Adam Taras <ataras2@gmail.com>
2022-10-04 21:08:44 +1100

    added evaluator placeholders

---
+++ NF/torch/NFEvaluator.py
@@ -0,0 +1,30 @@
+
+
+class NFEvaluator:
+    def __init__(self, model):
+        pass
+
+    def get_results(self):
+        # compute results as in UvA tute
+        pass
+    
+    def standard_interp(self, save=False):
+        # interp between some samples 
+        pass
+
+    def interp_inside_out(self, save=False):
+        # interp out from mean to some fixed distance out through training data
+        pass
+    
+    def interp_inside_out_rand_dir(self, save=False):
+        # pick a random direction and interp out from it for a fixed distance
+        pass
+    
+    def show_random_samples(self):
+        # create some images from the model
+        pass
+    
+    def hist_of_training_imgs(self):
+        pass
+
+    
